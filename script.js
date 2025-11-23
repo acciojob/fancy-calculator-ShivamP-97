5eff7433353e5a8fdae2919bfb6f62939036d7d6
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-23 22:56:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let firstInput = prompt("Enter the first number:");
+firstInput = parseInt(firstInput);
+
+let multiplied = firstInput * 2;
+
+let secondInput = prompt("Enter the second number:");
+secondInput = parseInt(secondInput);
+
+let result = multiplied + secondInput;
+
+alert("The final result is: " + result);
